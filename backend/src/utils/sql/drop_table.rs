a94e96fe830558f73728fe9d023722e799333f1d
SHIMA0111 <seigo.oshima@little-tabby.com>
2024-06-06 01:25:31 +0900

    implement drop_tables

--- backend/src/utils/sql/drop_table.rs
+++ backend/src/utils/sql/drop_table.rs
@@ -1,7 +1,7 @@
 use log::{error, info, warn};
 use crate::data::connector::DBConnection;
 use crate::utils::error::TimeKeeperError::{DBConnectionException, InvalidSettingException};
-use crate::utils::sql::SCHEMA_NAME;
+use crate::utils::sql::{get_constraint_name, SCHEMA_NAME};
 use crate::utils::sql::update::toggle_category_setting_for_table;
 use crate::utils::types::TimeKeeperResult;
 
@@ -31,7 +31,19 @@ pub(crate) async fn drop_table(table_name: &str, conn: &mut DBConnection) -> Tim
 
     let query_foreign_table_stmt = format!(
         "SELECT table_name FROM {}.category_setting WHERE superior_table=$1", SCHEMA_NAME);
+    let on_table_rows =
+        transaction.query(&query_foreign_table_stmt, &[&table_name]).await;
+    if let Err(e) = on_table_rows {
+        error!("Failed to get data of relation between tables because [{:?}]", e);
+        return Err(DBConnectionException("Cannot get relation between table.".to_string()));
+    }
+    let on_table_rows = on_table_rows.unwrap();
+    for row in &on_table_rows {
+        let dist_table_name: &str = row.get("table_name");
+        let constraint_name = get_constraint_name(dist_table_name, table_name);
 
+        let foreign_stmt = format!("UPDATE {}.{} SET superior_category_id=NULL", SCHEMA_NAME, dist_table_name);
+    }
 
     Ok(())
 }
