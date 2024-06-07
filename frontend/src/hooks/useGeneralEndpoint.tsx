617689ff518260cb07ac5bf064466e388f78efac
SHIMA0111 <shima@little-tabby.com>
2024-06-08 06:50:00 +0900

    improve general endpoint

--- frontend/src/hooks/useGeneralEndpoint.tsx
+++ frontend/src/hooks/useGeneralEndpoint.tsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import {useMemo} from "react";
 
 export const useGeneralEndpoint = (backend_url: string) => {
-    const axiosGeneralEndpoint = axios.create({
-        baseURL: `${backend_url}/v1/general`,
-        timeout: 5000
-    });
+    const axiosGeneralEndpoint = useMemo(() => {
+        const url = backend_url.endsWith("/") ? backend_url.slice(0, -1) : backend_url;
+        return axios.create({
+            baseURL: `${url}/v1/general`,
+            timeout: 5000
+        })
+    }, [backend_url]);
     
     return axiosGeneralEndpoint;
 }
